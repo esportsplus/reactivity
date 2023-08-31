df2786114fa32a05d07bc289f194c79a3e23d0f7
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-08-30 20:34:07 -0700

    feat: export constants

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { default as macro } from './macro';
 export { default as resource } from './resource';
 export { default as reactive } from './reactive';
 export { computed, dispose, effect, root, signal } from './signal';
+export * from './constants';
 export * from './types';
