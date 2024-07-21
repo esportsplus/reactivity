fc578ef8e2dbb7ffa291c402019f79d8e1beaba9
Israel Casillas Jr <ICJR@users.noreply.github.com>
2024-07-20 23:33:04 -0700

    fix: reactive type guard

--- src/reactive/index.ts
+++ src/reactive/index.ts
@@ -10,7 +10,9 @@ type Guard<T> =
         ? { [K in keyof T]: Never<K, Guard<T[K]>> }
         : T extends unknown[]
             ? T
-            : never;
+            : T extends Function
+                ? never
+                : T;
 
 type Infer<T> =
     T extends (...args: unknown[]) => unknown
@@ -34,7 +36,7 @@ export default <T>(data: Guard<T>, options: Options = {}) => {
         value = object(data as { [K in keyof T]: T[K] }, options);
     }
     else {
-        throw new Error('Reactivity: received invalid input for `reactive()`', data);
+        throw new Error(`Reactivity: 'reactive' received invalid input - ${JSON.stringify(data)}`);
     }
 
     return value as T extends Record<PropertyKey, unknown> ? { [K in keyof T]: Infer<T[K]> } : Infer<T>;
